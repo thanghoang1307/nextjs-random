a7e956371caf9847ccd9176ad299c2461cee2cc5
TeeGee <thang.hoang1307@gmail.com>
2022-06-03 11:51:47 +0700

    update

--- pages/api/update-code.js
+++ pages/api/update-code.js
@@ -4,24 +4,12 @@ import excuteQuery from '../../middleware/mysql-db';
 
 export default async (req, res) => {
     try {
-        console.log("req nom", req.body)
       const result = await excuteQuery({
           query: 'INSERT INTO code (code, seri) VALUES(?)',
           values: [[req.body.code, req.body.seri]],
       });
-      console.log( "ttt",result );
       res.json({message: "done"});
   } catch ( error ) {
       console.log( error );
   }
   };
-
-// const handler = nextConnect();
-// handler.use(middleware);
-// handler.post(async (req, res) => {
-//     let data = req.body;
-//     let doc = await req.db.collection('code').insertOne(data);
-//     res.json(doc);
-// });
-
-// export default handler;
